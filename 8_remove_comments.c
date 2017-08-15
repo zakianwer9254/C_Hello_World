4e2ffb4e204dc81ccc9694a983299df612969556
Zaki <anwer_zaki@rediffmail.com>
2017-08-16 00:47:34 +0530

    input of exression

--- 8_remove_comments.c
+++ 8_remove_comments.c
@@ -1,33 +1,131 @@
-#include<stdio.h>
-#define MAXLENTH 1000
-/*what is this***/
+/* Singly Linked List Implementation */
 
-int main(){
-	int c;
-	while((c=getchar())!=EOF){
-		if(c=='"'){
-			printf("%c",c);
-			while((c=getchar())!='"')
-				printf("%c",c);
-		}
-		else if(c=='/'){
-			if((c=getchar())=='/'){
-				while((c=getchar())!='\n');
-			}
-			else if(c=='*'){
-				long_comment();
-			}
-		}
-		else
-			printf("%c",c);
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node{
+	int value;
+	struct node *next;
+};
+
+struct node *head = NULL;
+
+
+
+
+void insertNode(int key){
+	struct node *temp = (struct node * )malloc(sizeof(struct node));
+
+	if(head==NULL){
+		temp->value=key;
+		temp->next = NULL;
+		head=temp;
 	}
-
+	else{
+		struct node *p = head;
+		p=head;
+		while(p->next!=NULL){
+			p=p->next;
+		}
+		temp->value=key;
+		temp->next = NULL;
+		p->next=temp;
+	}
 }
 
-void long_comment(){
-	int c;
-	while((c=getchar())!='*');
-	if(c!='/')
-		long_comment();
-	return;
+void traverse(){
+	struct node *p = head;
+	while(p!=NULL){
+		printf("%d ",p->value);
+		p=p->next;
+	}
 }
+
+
+
+
+
+
+void deleteNode(int key){
+	struct node *p = head;
+	if(p->value==key){
+		head=p->next;
+		free(p);
+		printf("node deleted\n");
+	}
+	else{
+		while(p->next!=NULL){
+			if(p->next->value==key){
+				struct node *q = (struct node*)malloc(sizeof(struct node*));
+				q=p->next;
+				p->next=q->next;
+				free(q);
+				printf("node deleted\n");
+				break;
+			}
+			p=p->next;
+		}
+	}
+}
+
+bool isPresent(key){
+	struct node *p = head;
+	if(p->value==key){
+		return true;
+	}
+	else{
+		while(p->next!=NULL){
+			if(p->next->value==key){
+				return true;
+			}
+			p=p->next;
+		}
+	}
+	return false;
+}
+
+int getElement(int index){
+	struct node *p = (struct node*)malloc(sizeof(struct node));
+	p=head;
+	if(index==0 && p!=NULL){
+		return p->value;
+	}
+	else{
+		for(int i=0; i<index && p!=NULL; i++){
+			p=p->next;
+		}
+		return p->value;
+	}
+}
+
+int main()
+{
+	
+	printf("Function Specifications are : \n");
+   	printf("insertNode(key)\n");
+   	printf("deleteNode(key)\n");
+   	printf("isPresent(key)\n");
+   	printf("traverse()\n");
+   	printf("getElement(index)\n");
+   	insertNode(5);
+   	insertNode(6);
+   	insertNode(9);
+   	insertNode(8);
+   	traverse();
+   	deleteNode(8);
+   	traverse();
+   	if(isPresent(7))
+   		printf("Yes it exist\n");
+   	else
+   		printf("not exist\n");
+   	//isPresent(9);
+   	//isPresent(7);
+   	//printf("%d",getElement(2));
+   	printf("%d",getElement(3));
+   	//printf("%d",getElement(4));
+}
+
+
---
+++ getInput.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+
+
+int buffer=0;
+
+int main(){
+	char *s=(char*)malloc(sizeof(char));
+	int ans;
+	while((ans=get(s))!=EOF){
+		switch(ans){
+			case '0' :
+			printf("%d\n",atoi(s));
+			break;
+			case '+' :
+			printf("its +\n");
+			break;
+			case '-' :
+			printf("its minus\n");
+			break;
+		}
+	}
+
+
+}
+
+int get(char *s){
+	int i=0,c;
+	int size=1,flag=0;
+	while((s[0]=c=getch())==' ' || c==' ' || c=='\t');
+	s = (char *)realloc(s,++size*sizeof(char));
+	s[1]='\0';
+	if(!isdigit(c))
+		return c;
+	if(isdigit(c)){
+		s = realloc(s,++size*sizeof(char));
+		while(isdigit(s[++i]=c=getch()))
+			s = realloc(s,++size*sizeof(char));
+		
+	}
+	s[i]='\0';
+	if(c!=EOF)
+		ungetch(c);
+	return '0';
+}
+
+void getch(){
+	int x;
+	if(buffer!=0){
+		x=buffer;
+		buffer=0;
+		return x;
+	}
+	else
+		return getchar();
+}
+
+void ungetch(c){
+	buffer=c;
+}
